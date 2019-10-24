f875387b60e8b0940b4cc4dab7d0294436a0095c
Axel Johansson <axel.johansson@siilim191010.local>
2019-10-24 17:43:26 +0300

    call story api

--- story.js
+++ story.js
@@ -1,3 +1,13 @@
-setTimeout(() => {
-  document.getElementById("story").innerHTML = "Tähän tarina."
-}, 3000);
+$(document).ready(() => {
+  const url = "https://fgrhn2wld3.execute-api.eu-north-1.amazonaws.com/dev/story";
+  $.ajax({
+    url: url,
+    type: "GET",
+    success: (res) => {
+      document.getElementById("story").innerHTML = "Tähän tarina."
+    },
+    error: (err) => {
+      console.log(`Error: ${err}`)
+    }
+  })
+});
